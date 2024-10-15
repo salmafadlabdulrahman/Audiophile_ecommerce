f29d6bf81e60996d1d19e29e926794e794180fe2
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-10-15 22:51:21 +0300

    update navbar position

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,20 +4,13 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="bg-black px-8">
-      <ul className="hidden md:flex items-center gap-3">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/headphones"}>Headphones</Link>
-        <Link href={"/speakers"}>Speakers</Link>
-        <Link href={"/earphones"}>Earphones</Link>
-      </ul>
-
-      <div className="flex items-center justify-between md:hidden border-b-2 border-[#c9c9c9] py-8">
+      <div className="flex items-center justify-between border-b-2 border-[#c9c9c9] py-8">
         <Image
           src="/assets/shared/mobile/icon-hamburger.svg"
           alt="hamburger icon"
           width={"15"}
           height={"15"}
-          className="cursor-pointer"
+          className="cursor-pointer md:hidden"
         />
         <Link href={"/"}>
           <Image
@@ -27,6 +20,13 @@ const Navbar = () => {
             height={"130"}
           />
         </Link>
+
+        <ul className="hidden md:flex items-center gap-3 text-white">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/headphones"}>Headphones</Link>
+          <Link href={"/speakers"}>Speakers</Link>
+          <Link href={"/earphones"}>Earphones</Link>
+        </ul>
         <Image
           src={"/assets/shared/mobile/icon-cart.svg"}
           height={"20"}
