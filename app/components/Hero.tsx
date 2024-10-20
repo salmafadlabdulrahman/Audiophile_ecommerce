908dfc41e3b3d2a879634ee2745ad39c9997f650
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-10-20 19:36:13 +0300

    update hero size

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,10 +16,10 @@ const Hero = () => {
       <div className=" text-center absolute top-20 right-[50%] translate-x-[50%] md:translate-x-[0] m-auto md:static md:m-0 md:text-left md:pl-4">
         {" "}
         <h5 className="uppercase text-darkgray tracking-[10px]">New Product</h5>
-          <h2 className="uppercase text-white text-[2em] tracking-wider leading-[1.1em] font-semibold mt-5">
+          <p className="uppercase text-white text-[2em] sm:text-[2.5em] md:text-[3em] break-words tracking-wider leading-[1.1em] font-semibold mt-5">
             XX99 Mark II
             <br /> headphones
-          </h2>
+          </p>
         <p className="text-darkgray m-auto mt-3 font-light tracking-tight text-[1em] md:m-0 md:mt-3 md:w-[70%]">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
@@ -34,3 +34,4 @@ const Hero = () => {
 
 export default Hero;
 
+/*text-[2em] */
