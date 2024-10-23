612dd5464466059f698670384cd2f31c2b591d97
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-10-23 15:28:48 +0300

    add stylings to the categories section

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Categories = () => {
   return (
     <div className="mt-[6em]">
-      <div className="flex xs:flex-col sm:flex-row justify-center items-center gap-2 border relative">
+      <div className="flex xs:flex-col sm:flex-row justify-center items-center gap-2 relative">
         {allCategories.map((category, i) => (
           <div className="relative" key={i}>
             <Image
@@ -15,9 +15,21 @@ const Categories = () => {
               height={150}
               className="z-20 absolute top-[0px] right-[50%] translate-x-[50%]"
             />
-            <div className="bg-gray mt-[3.5em] h-[150px] w-[200px] rounded-lg flex flex-col items-center justify-center">
-              <div className="text-center ">
-                <p>{category.categorie}</p>
+            <div className="bg-gray mt-[3.5em] mb-5 h-[150px] w-[200px] rounded-lg flex flex-col items-center justify-center">
+              <div className="text-center mt-[2em] ">
+                <p className="uppercase tracking-wide font-semibold">
+                  {category.categorie}
+                </p>
+                <p className="flex items-center justify-center mt-2 gap-2 text-[#aaaaaa] hover:text-orange cursor-pointer uppercase tracking-wider text-[15px] font-semibold">
+                  Shop{" "}
+                  <Image
+                    src={"/assets/shared/desktop/icon-arrow-right.svg"}
+                    width={9}
+                    height={9}
+                    alt="right arrow"
+                    className=""
+                  />
+                </p>
               </div>
             </div>
           </div>
@@ -27,3 +39,4 @@ const Categories = () => {
   );
 };
 export default Categories;
+
