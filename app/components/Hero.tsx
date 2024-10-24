db22ba41e1d88c5882d68f14525bebf58e2c1c7e
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-10-24 15:34:30 +0300

    update the background image

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,13 @@
 const Hero = () => {
   return (
-    <div className="bg-black w-full h-screen bg-[url(/assets/home/mobile/image-hero.png)] md:bg-[url(/assets/home/tablet/image-hero.png)] lg:bg-[url(/assets/home/desktop/image-hero.jpg)] bg-no-repeat bg-center ">
-      <div className="text-center">
+    <div className="bg-black w-full h-screen bg-[url(/assets/home/mobile/image-hero.png)] md:bg-[url(/assets/home/tablet/image-hero.png)] lg:bg-[url(/assets/home/desktop/image-hero.jpg)] bg-no-repeat bg-center bg-contain ">
+      <div className="text-center pb-5">
         <h5 className="uppercase text-darkgray tracking-[7px] font-semibold text-[1em] pt-[9em]">
           New Product
         </h5>
-        <p className="uppercase text-white mt-5 text-[1.7em] font-semibold leading-[1.1em] ">
-          XX99 Mark II <br />headphones
+        <p className="uppercase text-white mt-5 text-[1.9em] font-semibold leading-[1.1em] ">
+          XX99 Mark II <br />
+          headphones
         </p>
         <p className="text-darkgray font-light p-3 max-w-[70%] m-auto pt-4 md:w-[50%]">
           Experience natural, lifelike audio and exceptional build quality made
@@ -21,4 +22,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
