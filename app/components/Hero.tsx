baab8cb786cc8f91f7931dedd8c4509dabf87ced
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-10-24 15:58:09 +0300

    style the desktop version

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,15 @@
 const Hero = () => {
   return (
     <div className="bg-black w-full h-screen bg-[url(/assets/home/mobile/image-hero.png)] md:bg-[url(/assets/home/tablet/image-hero.png)] lg:bg-[url(/assets/home/desktop/image-hero.jpg)] bg-no-repeat bg-center bg-contain ">
-      <div className="text-center pb-5">
+      <div className="text-center pb-5 lg:text-left lg:pl-5 lg:w-[80%] lg:m-auto">
         <h5 className="uppercase text-darkgray tracking-[7px] font-semibold text-[1em] pt-[9em]">
           New Product
         </h5>
-        <p className="uppercase text-white mt-5 text-[2.7em] sm:text-[3em] font-semibold leading-[1.1em] ">
+        <p className="uppercase text-white mt-5 text-[2.7em] sm:text-[3.5em] font-semibold leading-[1.1em] ">
           XX99 Mark II <br />
           headphones
         </p>
-        <p className="text-darkgray font-light p-3 max-w-[70%] m-auto pt-4 md:w-[50%]">
+        <p className="text-darkgray font-light max-w-[70%] m-auto lg:m-0  pt-5 md:w-[50%] lg:w-[40%]">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </p>
