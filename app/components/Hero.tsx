8917d95abe9167af9812dc0a16f15d85ffe08834
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-10-24 15:17:40 +0300

    update the background img for the hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,26 +1,15 @@
-import Image from "next/image";
-
 const Hero = () => {
   return (
-    <div className="bg-black relative md:flex justify-around items-center">
-      <div className="order-2">
-        <Image
-          src={"/assets/home/mobile/image-hero.png"}
-          alt="black headphones"
-          height={"400"}
-          width={"400"}
-          className="m-auto"
-        />
-      </div>
-
-      <div className=" text-center absolute top-20 right-[50%] translate-x-[50%] md:translate-x-[0] m-auto md:static md:m-0 md:text-left md:pl-4">
-        {" "}
-        <h5 className="uppercase text-darkgray tracking-[10px]">New Product</h5>
-          <p className="uppercase text-white text-[2em] sm:text-[2.5em] md:text-[3.5em] break-words tracking-wider leading-[1.1em] font-semibold mt-5">
-            XX99 Mark II
-            <br /> headphones
-          </p>
-        <p className="text-darkgray m-auto mt-3 font-light tracking-tight text-[1em] md:m-0 md:mt-3 md:w-[70%]">
+    <div className="bg-black w-[100vw] h-screen bg-[url(/assets/home/mobile/image-hero.png)] md:bg-[url(/assets/home/tablet/image-hero.png)] lg:bg-[url(/assets/home/desktop/image-hero.jpg)] bg-no-repeat bg-center ">
+      <div className="text-center">
+        <h5 className="uppercase text-darkgray tracking-[7px] font-semibold text-[1em] pt-[9em]">
+          New Product
+        </h5>
+        <p className="uppercase text-white mt-5 text-[3em] font-semibold leading-[1.1em] ">
+          XX99 Mark II
+          <br /> headphones
+        </p>
+        <p className="text-darkgray font-light p-3 max-w-[70%] m-auto pt-4 md:w-[50%]">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </p>
